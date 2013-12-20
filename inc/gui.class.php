ad2f76ac00377ddddf484b77b3316d609ccb689d
sergej.mueller <sergej.mueller@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-20 10:19:16 +0000

    Changes in language files
    git-svn-id: https://plugins.svn.wordpress.org/antispam-bee/trunk@826018 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/gui.class.php
+++ inc/gui.class.php
@@ -246,7 +246,7 @@ class Antispam_Bee_GUI extends Antispam_Bee {
 								<input type="checkbox" name="ab_dnsbl_check" id="ab_dnsbl_check" value="1" <?php checked($options['dnsbl_check'], 1) ?> />
 								<label for="ab_dnsbl_check">
 									<?php esc_html_e('Use a public antispam database', 'antispam_bee') ?>
-									<span><?php _e('Matching the ip address with <a href="http://opm.tornevall.org" target="_blank">Tornevall</a>', 'antispam_bee') ?></span>
+									<span><?php _e('Matching the ip address with <a href="https://dnsbl.tornevall.org" target="_blank">Tornevall</a>', 'antispam_bee') ?></span>
 								</label>
 							</li>
 
